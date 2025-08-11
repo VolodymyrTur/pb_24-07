20a38a687fb103022e88ab52714fc63cbecc4e3d
vova <1989turvova@gmail.com>
2025-08-11 18:56:37 +0300

    feat: add lesson_2.py with examples of data types and basic operations

---
+++ lesson_2.py
@@ -0,0 +1,67 @@
+# immutable
+# str String
+# int Integer
+# float 5.3
+# decimal
+# bool Boolean
+# tuple
+# complex
+
+# mutable
+# list List
+# dict Dictionary
+# set
+# None
+
+# some = [1]
+# some2 = [1]
+#
+# print(id(some), id(some2))
+#
+# some_string = 1
+
+# a = [1]
+# b = [1]
+# c = b
+# print(id(a), id(b), id(c))
+
+# b = 16
+# c = 3
+
+# print(b // c)
+# print(b % c)
+# a = divmod(b, c)
+# a = divmod(15, 3)
+# print(a, b)
+# print(a)
+# print(help(divmod))
+
+# a = 9
+# print(a ** 0.5)
+
+# ''
+# ""
+# ''''''
+# """"""
+
+# greetings = "I'm"
+# name = "Vova"
+# name = 3
+#
+# string_formatted = f"{greetings} {name}"
+# string_formatted2 = "{} {}".format(greetings, name)
+# string_formatted3 = "%s %s" % (greetings, name)
+
+#
+# # print(greetings + " " + name)
+# print(string_formatted)
+# print(string_formatted2)
+# print(string_formatted3)
+
+# print(id(None))
+
+# input_number = input("Enter number:")
+# input_number2 = input("Enter number2:")
+
+# print(type(input_number))
+# print(int(input_number) + int(input_number2))
